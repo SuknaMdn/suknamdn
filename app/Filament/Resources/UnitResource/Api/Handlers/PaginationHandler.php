eb7127cfbdbff4b62a3ad9a2486c614e4b9d9dab
mohamed elshamy <drawinglifestyle6@gmail.com>
2024-12-19 15:49:38 +0200

    api filter edit

--- app/Filament/Resources/UnitResource/Api/Handlers/PaginationHandler.php
+++ app/Filament/Resources/UnitResource/Api/Handlers/PaginationHandler.php
@@ -18,41 +18,17 @@ class PaginationHandler extends Handlers {
         $model = static::getModel();
 
         // Apply min/max filters for price and space
-        if (request()->has('price_min')) {
+        if (request()->has('price_min') && request()->price_min != null) {
             $query->where('total_amount', '>=', request('price_min'));
         }
-        if (request()->has('price_max')) {
+        if (request()->has('price_max') && request()->price_max != null) {
             $query->where('total_amount', '<=', request('price_max'));
         }
-        if (request()->has('space_min')) {
+        if (request()->has('space_min') && request()->space_min != null) {
             $query->where('total_area', '>=', request('space_min'));
         }
-        if (request()->has('space_max')) {
+        if (request()->has('space_max') && request()->space_max != null) {
             $query->where('total_area', '<=', request('space_max'));
-        }
-
-        // Handle sorting by allowed sorts
-        $sort = request()->query('sort'); // Assuming you send the sort option as a query parameter (e.g., 'sort=price_asc')
-
-        if ($sort) {
-            switch ($sort) {
-                case 'price_asc':
-                    $query->orderBy('total_amount', 'asc'); // Sort by lowest price
-                    break;
-                case 'price_desc':
-                    $query->orderBy('total_amount', 'desc'); // Sort by highest price
-                    break;
-                case 'space_asc':
-                    $query->orderBy('total_area', 'asc'); // Sort by smallest area
-                    break;
-                case 'space_desc':
-                    $query->orderBy('total_area', 'desc'); // Sort by largest area
-                    break;
-                default:
-                    // Handle any other sorting options that are not specifically defined
-                    // You can add a default sorting option if needed
-                    $query->orderBy('created_at', 'desc');
-            }
         }
 
         $query = QueryBuilder::for($query)
@@ -61,7 +37,7 @@ class PaginationHandler extends Handlers {
             }])
             ->select('id', 'title', 'slug','case', 'building_number', 'floor', 'unit_type', 'unit_number', 'bedrooms', 'bathrooms', 'status', 'project_id', 'created_at', 'total_amount', 'total_area')
             ->allowedFields($this->getAllowedFields() ?? [])
-            ->allowedSorts($this->getAllowedSorts() ?? [])
+            // ->allowedSorts($this->getAllowedSorts() ?? [])
             ->allowedFilters($this->getAllowedFilters() ?? [])
             ->allowedIncludes($this->getAllowedIncludes() ?? [])
             ->paginate(request()->query('per_page'))
@@ -73,23 +49,6 @@ class PaginationHandler extends Handlers {
                 $firstImage = $item->images->first();
                 $item->image = asset('storage/' . $firstImage->image_path); // Adjust path as needed
             }
-
-            // // Handle additional features images
-            // if ($item->additionalFeatures()->exists()) {
-            //     $item->additionalFeatures = $item->additionalFeatures->map(function ($additionalFeature) {
-            //         $additionalFeature->icon = asset('storage/' . $additionalFeature->icon);  // Adjust path as needed
-            //         return $additionalFeature;
-            //     });
-            // }
-
-            // // Handle after sales services images
-            // if ($item->afterSalesServices()->exists()) {
-            //     $item->afterSalesServices = $item->afterSalesServices->map(function ($afterSalesService) {
-            //         $afterSalesService->icon = asset('storage/' . $afterSalesService->icon);  // Adjust path as needed
-            //         return $afterSalesService;
-            //     });
-            // }
-
             return $item;
         });
 
@@ -100,7 +59,7 @@ class PaginationHandler extends Handlers {
 
     public function getAllowedSorts(): array
     {
-        return ['id', 'title', 'status', 'project_id', 'created_at', 'price_asc', 'price_desc', 'space_asc', 'space_desc'];
+        return ['id', 'project_id', 'price_amount', 'total_space' , 'created_at'];
     }
 
     public function getAllowedFilters(): array
