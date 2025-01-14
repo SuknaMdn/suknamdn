6b45e9ac90543f2654338d781c390c947f11e329
mohamed elshamy <drawinglifestyle6@gmail.com>
2025-01-14 06:48:52 +0200

    setting add

--- database/settings/2024_02_21_153249_create_pages_settings.php
+++ database/settings/2024_02_21_153249_create_pages_settings.php
@@ -6,8 +6,8 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        // $this->migrator->add('general.term_and_condition', 700);
-        // $this->migrator->add('general.privacy_policy', 7);
-        // $this->migrator->add('general.about', 7);
+        $this->migrator->add('general.term_and_condition', 700);
+        $this->migrator->add('general.privacy_policy', 7);
+        $this->migrator->add('general.about', 7);
     }
 };
