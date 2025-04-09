3ed0ae2e9cca2cf2fc5d1a5206bfca76a2f1b573
mohamed elshamy <drawinglifestyle6@gmail.com>
2025-04-09 18:31:35 +0200

    PaginationHandler

--- app/Filament/Resources/BannerResource/Api/Handlers/PaginationHandler.php
+++ app/Filament/Resources/BannerResource/Api/Handlers/PaginationHandler.php
@@ -19,7 +19,7 @@ class PaginationHandler extends Handlers {
 
         $query = QueryBuilder::for($query)
         ->select('id', 'title', 'description', 'is_visible', 'sort','click_url_target', 'click_url')
-        // ->with('media')
+        ->with('media')
         ->with(['media' => function ($query) {
             $query->select('id', 'disk', 'model_type', 'model_id', 'conversions_disk', 'generated_conversions', 'file_name', 'collection_name');
         }])
